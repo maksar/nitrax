a93061603e2b21215a158c3acf7fa5f5397505b0
Shestakov, Aleksandr <a.shestakov@itransition.com>
2020-11-24 14:13:04 +0300

    TCI-2 Adding instagram service

--- fleet.nix
+++ fleet.nix
@@ -18,7 +18,7 @@
     { config, pkgs, ... }:
     {
       rootfs = "btrfs";
-      imports = [ ./hardware/efi.nix ./os ./modules/trimmer.nix ./modules/gitman.nix ./modules/nine11.nix ./modules/ldap-bot.nix ];
+      imports = [ ./hardware/efi.nix ./os ./modules/trimmer.nix ./modules/gitman.nix ./modules/nine11.nix ./modules/ldap-bot.nix ./modules/instagram.nix ];
 
       networking.hostName = "decepticons";
       deployment.targetHost = "decepticons.itransition.corp";
---
+++ modules/instagram.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+let
+  instagram = (builtins.getFlake "git+ssh://git@git.itransition.com:7999/workplace/instagram.git").defaultPackage.${pkgs.system};
+in
+{
+  environment.systemPackages = [ instagram ];
+
+  deployment.keys.instagram-env = {
+    text = builtins.readFile ~/projects/workplace/instagram/.env.production;
+  };
+  systemd.services.instagram = {
+    after = [ "instagram-env-key.service" ];
+    wants = [ "instagram-env-key.service" ];
+    script = ''
+      source <(sed -E 's/([A-Z_0-9]+)=(.*)/export \1=\2/g' /run/keys/instagram-env)
+      exec ${instagram}/bin/instagram
+    '';
+  };
+  systemd.timers.instagram = {
+    timerConfig = {
+      OnBootSec = "1d";
+      OnUnitInactiveSec = "1d";
+    };
+    wantedBy = [ "timers.target" ];
+  };
+}
